86194d2f819f2b674e61f82eb4325bece03925df
Nur-aly <chiketaevn@gmail.com>
2020-09-07 19:03:36 +0600

    allowed hosts

--- store/urls.py
+++ store/urls.py
@@ -22,9 +22,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', include('products.urls')),
     path('order/', include('orders.urls')),
     path('cart/', include('cart.urls')),
-    path('products/', include('products.urls'))
 ]
 
 
